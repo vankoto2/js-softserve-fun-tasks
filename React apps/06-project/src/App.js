ee9062ca626eed6a0a97d61f08b9ff3ebcc73866
Ivan Veselinov Georgiev <it.work.bulgaria@gmail.com>
2022-02-01 15:36:01 +0200

    using React.memo()

--- React apps/06-project/src/App.js
+++ React apps/06-project/src/App.js
@@ -1,19 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 
-import Button from "./components/UI/Button/Button"
+import Button from "./components/UI/Button/Button";
 import "./App.css";
+import DemoOutput from "./components/Demo/DemoOutput";
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
-const toggleParagraphHandler = () => {
-  setShowParagraph(prevShowParagraph => !prevShowParagraph)
-}
+  const toggleParagraphHandler = useCallback(() => {
+    console.log(allowToggle);
+    if (allowToggle) {
+      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+    }
+  }, [allowToggle]);
+
+  const allawToggleHandler = () => {
+    setAllowToggle(true)
+  }
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showParagraph && <p>This is new!</p>}
+      <DemoOutput show={showParagraph} />
+      <Button onClick={allawToggleHandler}>allowToggle</Button>
       <Button onClick={toggleParagraphHandler}>Show paragraph</Button>
     </div>
   );
