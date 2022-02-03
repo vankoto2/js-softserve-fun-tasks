f8d362144ad101ad37322019ebe2856a5bcf60ac
Ivan Veselinov Georgiev <it.work.bulgaria@gmail.com>
2022-02-03 18:25:43 +0200

    added user input value

--- React apps/11-project/src/components/SimpleInput.js
+++ React apps/11-project/src/components/SimpleInput.js
@@ -1,9 +1,39 @@
+import { useState, useRef } from "react";
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState("");
+
+  const nameInputStateHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmisionHandler = (event) => {
+    event.preventDefault();
+
+    if (enteredName.trim() === "") {
+      return;
+    }
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredName);
+
+    console.log(enteredValue);
+
+    // nameInputRef.current.value = "";
+    setEnteredName("");
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmisionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputStateHandler}
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
