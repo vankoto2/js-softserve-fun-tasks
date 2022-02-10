df8faa6178fcff427fc11342dff54c3771bc29bc
Ivan Veselinov Georgiev <it.work.bulgaria@gmail.com>
2022-02-10 12:24:35 +0200

    bug fix

--- React apps/13-project/src/store/index.js
+++ React apps/13-project/src/store/index.js
@@ -1,4 +1,4 @@
-import { createSlice, configureStore } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 
 import counterSlice from "./counter-slice";
 import authSlice from "./auth-slice";
