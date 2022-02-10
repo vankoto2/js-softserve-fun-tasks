b154b38022cb25103e988282b05b3b343a1807a4
Ivan Veselinov Georgiev <it.work.bulgaria@gmail.com>
2022-02-10 12:07:17 +0200

    bug fixing

--- React apps/13-project/src/App.js
+++ React apps/13-project/src/App.js
@@ -3,11 +3,10 @@ import { Fragment } from "react";
 import Counter from "./components/Counter";
 import Header from "./components/Header";
 import Auth from "./components/Auth";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import UserProfile from "./components/UserProfile";
 
 function App() {
-  const dispatch = useDispatch();
   const auth = useSelector((state) => state.auth.isAuthenticated);
 
   return (
