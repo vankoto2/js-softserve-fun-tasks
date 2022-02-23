26508f1cd174d59b8694ae065d1eda2bed95aea4
Ivan Veselinov Georgiev <it.work.bulgaria@gmail.com>
2022-02-23 12:30:12 +0200

    adding products

--- React apps/15-project/src/pages/Products.js
+++ React apps/15-project/src/pages/Products.js
@@ -1,10 +1,16 @@
+import { Link } from "react-router-dom";
+
 const Products = () => {
-  return <h1>The Products Page</h1>;
-  <ul>
-    <li>Product 1</li>
-    <li>Product 2</li>
-    <li>Product 3</li>
-  </ul>
+  return (
+    <section>
+      <h1>The Products Page</h1>
+      <ul>
+        <li><Link to="/products/book">A Book</Link></li>
+        <li><Link to="/products/carpet">A Carpet</Link></li>
+        <li><Link to="/products/online-course">An Online Course</Link></li>
+      </ul>
+    </section>
+  );
 };
 
 export default Products;
