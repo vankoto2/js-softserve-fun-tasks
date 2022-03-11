9d494f7b283aa2fd88c41636f7bc3f02a67daece
Ivan Veselinov Georgiev <it.work.bulgaria@gmail.com>
2022-03-11 11:06:53 +0200

    adding pages

---
+++ React apps/18-project-nextjs/pages/news/[newsId].js
@@ -0,0 +1,11 @@
+import { useRouter } from "next/router";
+
+function DetailsPage() {
+  const router = useRouter();
+
+  const newsId = router.query.newsId;
+
+  return <h1>The Details Page</h1>;
+}
+
+export default DetailsPage;
