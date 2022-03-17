2da1f3dc65d38bababf5aad5225466c12c160b35
Ivan Veselinov Georgiev <it.work.bulgaria@gmail.com>
2022-03-17 10:39:53 +0200

    project 20

---
+++ React apps/20-project/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+import "./App.css";
+import Modal from "./components/Modal/Modal";
+import Backdrop from "./components/Backdrop/Backdrop";
+import List from "./components/List/List";
+
+class App extends Component {
+  state = {
+    modalIsOpen: false,
+  };
+  
+
+  render() {
+    return (
+      <div className="App">
+        <h1>React Animations</h1>
+        <Modal />
+        <Backdrop />
+        <button className="Button">Open Modal</button>
+        <h3>Animating Lists</h3>
+        <List />
+      </div>
+    );
+  }
+}
+
+export default App;
